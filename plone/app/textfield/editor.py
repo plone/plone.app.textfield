3ae442605b77ef54b23419c98cf8582358ffeeda
Ross Patterson <me@rpatterson.net>
2010-03-17 21:20:25 +0000

    Fix the field schemata so they can be used as the form schema when adding the field using plone.schemaeditor
    svn path=/plone.app.textfield/trunk/; revision=35182

--- plone/app/textfield/editor.py
+++ plone/app/textfield/editor.py
@@ -1,4 +1,19 @@
+from zope.schema import interfaces as schema_ifaces
+
+from plone.app.textfield import interfaces
 from plone.app.textfield import RichText
 from plone.schemaeditor.fields import FieldFactory
+
+class IRichText(interfaces.IRichText, schema_ifaces.IFromUnicode):
+
+    default = RichText(
+        title=interfaces.IRichText['default'].title,
+        description=interfaces.IRichText['default'].description,
+        required=False)
+
+    missing_value = RichText(
+        title=interfaces.IRichText['missing_value'].title,
+        description=interfaces.IRichText['missing_value'].description,
+        required=False)
 
 RichTextFactory = FieldFactory(RichText, u'Rich Text')
