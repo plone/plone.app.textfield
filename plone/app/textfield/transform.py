77d166123c53ee7b7a030238ade417c73b01d2d6
Karl Johan Kleist <karl.johan@kleist-it-consulting.de>
2011-11-08 13:41:37 +0100

    If no transform path is found, return empty string and log error message (instead of throwing an exception in the face of the user).

--- plone/app/textfield/transform.py
+++ plone/app/textfield/transform.py
@@ -1,46 +1,50 @@
-from zope.interface import implements
-
-from zope.app.component.hooks import getSite
-from ZODB.POSException import ConflictError
+import logging
 
 from plone.app.textfield.interfaces import ITransformer, TransformError
-
 from Products.CMFCore.utils import getToolByName
+from ZODB.POSException import ConflictError
+from zope.app.component.hooks import getSite
+from zope.interface import implements
+
+LOG = logging.getLogger('plone.app.textfield')
 
 class PortalTransformsTransformer(object):
     """Invoke portal_transforms to perform a conversion
     """
-    
+
     implements(ITransformer)
-    
+
     def __init__(self, context):
         self.context = context
-    
+
     def __call__(self, value, mimeType):
-        
+
         # shortcut if we already have the right value
         if mimeType is value.mimeType:
             return value.output
-        
+
         site = getSite()
-        
+
         transforms = getToolByName(site, 'portal_transforms', None)
         if transforms is None:
             raise TransformError("Cannot find portal_transforms tool")
-        
+
         try:
             data = transforms.convertTo(mimeType,
                                         value.raw_encoded,
                                         mimetype=value.mimeType,
                                         context=self.context,
-                                        object=value._raw_holder, # portal_transforms caches on this object
+                                        # portal_transforms caches on this
+                                        object=value._raw_holder,
                                         encoding=value.encoding)
-            # TODO: "data == None" might indicate no existing transform path,
-            # do we really want this to throw an exception in the face of the user?
-            # Better to use an identity transform?
-            output = data.getData()
-            return output.decode(value.encoding)
+            if data is None:
+                LOG.error('No transform path found from "%s" to "%s".' %
+                          (value.mimeType, mimeType))
+                return u''; # TODO: error message instead?
+            else:
+                output = data.getData()
+                return output.decode(value.encoding)
         except ConflictError:
             raise
         except Exception, e:
-            raise TransformError("Error during transformation", e)
+            raise TransformError('Error during transformation', e)
