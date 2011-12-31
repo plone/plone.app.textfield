19561922c6355dfc639f345d62e297cbd459edab
David Glick <dglick@gmail.com>
2011-12-30 23:58:58 -0800

    simplify the settings for rich text fields in the TTW schema editor

--- plone/app/textfield/editor.py
+++ plone/app/textfield/editor.py
@@ -1,19 +1,32 @@
+from zope.interface import Attribute
 from zope.schema import interfaces as schema_ifaces
+from zope import schema
 
 from plone.app.textfield import interfaces
 from plone.app.textfield import RichText
 from plone.schemaeditor.fields import FieldFactory
 
+try:
+    import plone.app.vocabularies
+    HAS_VOCABS = True
+except ImportError:
+    HAS_VOCABS = False
+
+
 class IRichText(interfaces.IRichText, schema_ifaces.IFromUnicode):
 
-    default = RichText(
-        title=interfaces.IRichText['default'].title,
-        description=interfaces.IRichText['default'].description,
-        required=False)
+    if HAS_VOCABS:
+        default_mime_type = schema.Choice(
+            title = u'Input format',
+            vocabulary = 'plone.app.vocabularies.AllowedContentTypes',
+            default = 'text/html',
+            )
+    else:
+        default_mime_type = Attribute('')
 
-    missing_value = RichText(
-        title=interfaces.IRichText['missing_value'].title,
-        description=interfaces.IRichText['missing_value'].description,
-        required=False)
+    # prevent some settings from being included in the field edit form
+    output_mime_type = Attribute('')
+    allowed_mime_types = Attribute('')
+
 
 RichTextFactory = FieldFactory(RichText, u'Rich Text')
