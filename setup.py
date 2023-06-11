105340c15f44649e32bef5e7afd27b4fdbbea346
Gil Forcada Codinachs <gil.gnome@gmail.com>
2023-06-11 23:33:48 +0200

    chore: use pathlib

--- setup.py
+++ setup.py
@@ -1,15 +1,22 @@
+from pathlib import Path
 from setuptools import find_packages
 from setuptools import setup
 
 
 version = "2.0.1.dev0"
 
+long_description = (
+    f"{Path('README.rst').read_text()}\n{Path('CHANGES.rst').read_text()}"
+)
+
 setup(
     name="plone.app.textfield",
     version=version,
     description="Text field with MIME type support",
-    long_description=open("README.rst").read() + "\n" + open("CHANGES.rst").read(),
-    # Get more strings from https://pypi.org/classifiers/
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    # Get more strings from
+    # https://pypi.org/classifiers/
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Framework :: Plone",
