cbaafd2b707a4a93540b0d1b7d0f4d6e4e5ed9a7
Gil Forcada Codinachs <gil.gnome@gmail.com>
2024-01-21 11:12:21 +0100

    chore: adjust dependencies

--- setup.py
+++ setup.py
@@ -43,6 +43,7 @@ setup(
     install_requires=[
         "setuptools",
         "plone.base",
+        "Zope",
     ],
     extras_require={
         "portaltransforms": ["Products.PortalTransforms"],
